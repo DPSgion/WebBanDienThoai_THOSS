3ccdc5434db2a211c29d79e9de8119f3965c455e
Phuong <np384874@gmail.com>
2025-11-29 22:23:49 +0700

    Thiết kế cấu trúc trang admin

---
+++ project/admin/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style_admin.css">
+
+    <title>Admin Page</title>
+</head>
+<body>
+    <div class="header">
+        <div class="header-left">
+            <img src="../assets/images/logodt.png" width="80px">
+            <h1>Admin</h1>
+        </div>
+        <div class="header-right">
+            <p class="admin-name">Admin Name</p>
+            <button class="logout-btn">Đăng xuất</button>
+        </div>
+    </div>
+
+    <div class="sidebar">
+        <ul>
+            <li>
+                <a href="#">Dashboard</a>
+            </li>
+            <li>
+                <a href="#">Quản lý sản phẩm</a>
+            </li>
+            <li>
+                <a href="#">Quản lý đơn hàng</a>
+            </li>
+            <li>
+                <a href="#">Quản lý khách hàng</a>
+            </li>
+        </ul>
+    </div>
+    
+</body>
+</html>
