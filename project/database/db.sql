b5554312c2e13db3e76b07937ffe42353ef105f9
Phuong <np384874@gmail.com>
2025-12-04 16:19:54 +0700

    Cập nhật db: bảng sản phẩm (set khóa ngoại id_danh_muc not null thay vì null)

--- project/database/db.sql
+++ project/database/db.sql
@@ -1,143 +1,237 @@
--- =======================================
--- CSDL: He thong ban dien thoai
--- =======================================
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 04, 2025 at 09:16 AM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
 
-DROP DATABASE IF EXISTS bandienthoai;
-CREATE DATABASE bandienthoai;
-USE bandienthoai;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- =======================================
--- 1. BANG DANH MUC
--- =======================================
-CREATE TABLE danh_muc (
-    id_danh_muc INT AUTO_INCREMENT PRIMARY KEY,
-    ten_danh_muc VARCHAR(100) NOT NULL
-) ENGINE=InnoDB;
 
--- =======================================
--- 2. BANG SAN PHAM
--- =======================================
-CREATE TABLE san_pham (
-    id_san_pham INT AUTO_INCREMENT PRIMARY KEY,
-    id_danh_muc INT,
-    ten_san_pham VARCHAR(255) NOT NULL,
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-    cpu VARCHAR(100),
-    pin VARCHAR(50),
-    man_hinh VARCHAR(100),
-    os VARCHAR(50),
-    camera_truoc VARCHAR(50),
-    camera_sau VARCHAR(50),
+--
+-- Database: `bandienthoai`
+--
 
-    FOREIGN KEY (id_danh_muc) REFERENCES danh_muc(id_danh_muc)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE
-) ENGINE=InnoDB;
+-- --------------------------------------------------------
 
--- =======================================
--- 3. BANG ANH SAN PHAM
--- =======================================
-CREATE TABLE anh_san_pham (
-    id_anh INT AUTO_INCREMENT PRIMARY KEY,
-    id_san_pham INT,
-    duong_dan_anh VARCHAR(255) NOT NULL,
+--
+-- Table structure for table `anh_san_pham`
+--
 
-    FOREIGN KEY (id_san_pham) REFERENCES san_pham(id_san_pham)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-) ENGINE=InnoDB;
+DROP TABLE IF EXISTS `anh_san_pham`;
+CREATE TABLE IF NOT EXISTS `anh_san_pham` (
+  `id_anh` int NOT NULL AUTO_INCREMENT,
+  `id_san_pham` int DEFAULT NULL,
+  `duong_dan_anh` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_anh`),
+  KEY `id_san_pham` (`id_san_pham`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- =======================================
--- 4. BANG BIEN THE SAN PHAM
--- =======================================
-CREATE TABLE bien_the (
-    id_bien_the INT AUTO_INCREMENT PRIMARY KEY,
-    id_san_pham INT,
+-- --------------------------------------------------------
 
-    ram VARCHAR(20),
-    rom VARCHAR(20),
-    mau VARCHAR(50),
-    gia DECIMAL(10,2) NOT NULL,
-    so_luong_ton INT DEFAULT 0,
+--
+-- Table structure for table `bien_the`
+--
 
-    FOREIGN KEY (id_san_pham) REFERENCES san_pham(id_san_pham)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-) ENGINE=InnoDB;
+DROP TABLE IF EXISTS `bien_the`;
+CREATE TABLE IF NOT EXISTS `bien_the` (
+  `id_bien_the` int NOT NULL AUTO_INCREMENT,
+  `id_san_pham` int DEFAULT NULL,
+  `ram` varchar(20) DEFAULT NULL,
+  `rom` varchar(20) DEFAULT NULL,
+  `mau` varchar(50) DEFAULT NULL,
+  `gia` decimal(10,2) NOT NULL,
+  `so_luong_ton` int DEFAULT '0',
+  PRIMARY KEY (`id_bien_the`),
+  KEY `id_san_pham` (`id_san_pham`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- =======================================
--- 5. BANG NGUOI DUNG  (KHONG CO BANG VAI TRO)
--- =======================================
-CREATE TABLE nguoi_dung (
-    id_nguoi_dung INT AUTO_INCREMENT PRIMARY KEY,
-    ho_ten VARCHAR(100) NOT NULL,
-    sdt VARCHAR(20) UNIQUE,
-    mat_khau VARCHAR(255) NOT NULL,
-    vai_tro ENUM('admin', 'khachhang') DEFAULT 'khachhang'
-) ENGINE=InnoDB;
+-- --------------------------------------------------------
 
--- =======================================
--- 6. BANG GIO HANG
--- =======================================
-CREATE TABLE gio_hang (
-    id_gio_hang INT AUTO_INCREMENT PRIMARY KEY,
-    id_nguoi_dung INT,
-    ngay_tao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--
+-- Table structure for table `chi_tiet_don_hang`
+--
 
-    FOREIGN KEY (id_nguoi_dung) REFERENCES nguoi_dung(id_nguoi_dung)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-) ENGINE=InnoDB;
+DROP TABLE IF EXISTS `chi_tiet_don_hang`;
+CREATE TABLE IF NOT EXISTS `chi_tiet_don_hang` (
+  `id_ctdh` int NOT NULL AUTO_INCREMENT,
+  `id_don_hang` int DEFAULT NULL,
+  `id_bien_the` int DEFAULT NULL,
+  `so_luong` int NOT NULL,
+  `gia_luc_mua` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id_ctdh`),
+  KEY `id_don_hang` (`id_don_hang`),
+  KEY `id_bien_the` (`id_bien_the`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- =======================================
--- 7. BANG CHI TIET GIO HANG
--- =======================================
-CREATE TABLE gio_hang_chi_tiet (
-    id_chi_tiet INT AUTO_INCREMENT PRIMARY KEY,
-    id_gio_hang INT,
-    id_bien_the INT,
-    so_luong INT DEFAULT 1,
+-- --------------------------------------------------------
 
-    FOREIGN KEY (id_gio_hang) REFERENCES gio_hang(id_gio_hang)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
+--
+-- Table structure for table `danh_muc`
+--
 
-    FOREIGN KEY (id_bien_the) REFERENCES bien_the(id_bien_the)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-) ENGINE=InnoDB;
+DROP TABLE IF EXISTS `danh_muc`;
+CREATE TABLE IF NOT EXISTS `danh_muc` (
+  `id_danh_muc` int NOT NULL AUTO_INCREMENT,
+  `ten_danh_muc` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_danh_muc`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- =======================================
--- 8. BANG DON HANG
--- =======================================
-CREATE TABLE don_hang (
-    id_don_hang INT AUTO_INCREMENT PRIMARY KEY,
-    id_nguoi_dung INT,
-    trang_thai ENUM('choxuly','danggiao','hoanthanh','dahuy') DEFAULT 'choxuly',
-    tong_tien DECIMAL(10,2),
-    dia_chi VARCHAR(255),
-    ngay_dat TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--
+-- Dumping data for table `danh_muc`
+--
 
-    FOREIGN KEY (id_nguoi_dung) REFERENCES nguoi_dung(id_nguoi_dung)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-) ENGINE=InnoDB;
+INSERT INTO `danh_muc` (`id_danh_muc`, `ten_danh_muc`) VALUES
+(5, 'Xiaomi'),
+(6, 'Iphone'),
+(8, 'SSS');
 
--- =======================================
--- 9. BANG CHI TIET DON HANG
--- =======================================
-CREATE TABLE chi_tiet_don_hang (
-    id_ctdh INT AUTO_INCREMENT PRIMARY KEY,
-    id_don_hang INT,
-    id_bien_the INT,
-    so_luong INT NOT NULL,
-    gia_luc_mua DECIMAL(10,2) NOT NULL,
+-- --------------------------------------------------------
 
-    FOREIGN KEY (id_don_hang) REFERENCES don_hang(id_don_hang)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
+--
+-- Table structure for table `don_hang`
+--
 
-    FOREIGN KEY (id_bien_the) REFERENCES bien_the(id_bien_the)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-) ENGINE=InnoDB;
+DROP TABLE IF EXISTS `don_hang`;
+CREATE TABLE IF NOT EXISTS `don_hang` (
+  `id_don_hang` int NOT NULL AUTO_INCREMENT,
+  `id_nguoi_dung` int DEFAULT NULL,
+  `trang_thai` enum('choxuly','danggiao','hoanthanh','dahuy') DEFAULT 'choxuly',
+  `tong_tien` decimal(10,2) DEFAULT NULL,
+  `dia_chi` varchar(255) DEFAULT NULL,
+  `ngay_dat` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_don_hang`),
+  KEY `id_nguoi_dung` (`id_nguoi_dung`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gio_hang`
+--
+
+DROP TABLE IF EXISTS `gio_hang`;
+CREATE TABLE IF NOT EXISTS `gio_hang` (
+  `id_gio_hang` int NOT NULL AUTO_INCREMENT,
+  `id_nguoi_dung` int DEFAULT NULL,
+  `ngay_tao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_gio_hang`),
+  KEY `id_nguoi_dung` (`id_nguoi_dung`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gio_hang_chi_tiet`
+--
+
+DROP TABLE IF EXISTS `gio_hang_chi_tiet`;
+CREATE TABLE IF NOT EXISTS `gio_hang_chi_tiet` (
+  `id_chi_tiet` int NOT NULL AUTO_INCREMENT,
+  `id_gio_hang` int DEFAULT NULL,
+  `id_bien_the` int DEFAULT NULL,
+  `so_luong` int DEFAULT '1',
+  PRIMARY KEY (`id_chi_tiet`),
+  KEY `id_gio_hang` (`id_gio_hang`),
+  KEY `id_bien_the` (`id_bien_the`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nguoi_dung`
+--
+
+DROP TABLE IF EXISTS `nguoi_dung`;
+CREATE TABLE IF NOT EXISTS `nguoi_dung` (
+  `id_nguoi_dung` int NOT NULL AUTO_INCREMENT,
+  `ho_ten` varchar(100) NOT NULL,
+  `sdt` varchar(20) DEFAULT NULL,
+  `mat_khau` varchar(255) NOT NULL,
+  `vai_tro` enum('admin','khachhang') DEFAULT 'khachhang',
+  PRIMARY KEY (`id_nguoi_dung`),
+  UNIQUE KEY `sdt` (`sdt`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `san_pham`
+--
+
+DROP TABLE IF EXISTS `san_pham`;
+CREATE TABLE IF NOT EXISTS `san_pham` (
+  `id_san_pham` int NOT NULL AUTO_INCREMENT,
+  `id_danh_muc` int NOT NULL,
+  `ten_san_pham` varchar(255) NOT NULL,
+  `cpu` varchar(100) DEFAULT NULL,
+  `pin` varchar(50) DEFAULT NULL,
+  `man_hinh` varchar(100) DEFAULT NULL,
+  `os` varchar(50) DEFAULT NULL,
+  `camera_truoc` varchar(50) DEFAULT NULL,
+  `camera_sau` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_san_pham`),
+  KEY `id_danh_muc` (`id_danh_muc`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `anh_san_pham`
+--
+ALTER TABLE `anh_san_pham`
+  ADD CONSTRAINT `anh_san_pham_ibfk_1` FOREIGN KEY (`id_san_pham`) REFERENCES `san_pham` (`id_san_pham`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `bien_the`
+--
+ALTER TABLE `bien_the`
+  ADD CONSTRAINT `bien_the_ibfk_1` FOREIGN KEY (`id_san_pham`) REFERENCES `san_pham` (`id_san_pham`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `chi_tiet_don_hang`
+--
+ALTER TABLE `chi_tiet_don_hang`
+  ADD CONSTRAINT `chi_tiet_don_hang_ibfk_1` FOREIGN KEY (`id_don_hang`) REFERENCES `don_hang` (`id_don_hang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `chi_tiet_don_hang_ibfk_2` FOREIGN KEY (`id_bien_the`) REFERENCES `bien_the` (`id_bien_the`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `don_hang`
+--
+ALTER TABLE `don_hang`
+  ADD CONSTRAINT `don_hang_ibfk_1` FOREIGN KEY (`id_nguoi_dung`) REFERENCES `nguoi_dung` (`id_nguoi_dung`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `gio_hang`
+--
+ALTER TABLE `gio_hang`
+  ADD CONSTRAINT `gio_hang_ibfk_1` FOREIGN KEY (`id_nguoi_dung`) REFERENCES `nguoi_dung` (`id_nguoi_dung`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `gio_hang_chi_tiet`
+--
+ALTER TABLE `gio_hang_chi_tiet`
+  ADD CONSTRAINT `gio_hang_chi_tiet_ibfk_1` FOREIGN KEY (`id_gio_hang`) REFERENCES `gio_hang` (`id_gio_hang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `gio_hang_chi_tiet_ibfk_2` FOREIGN KEY (`id_bien_the`) REFERENCES `bien_the` (`id_bien_the`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `san_pham`
+--
+ALTER TABLE `san_pham`
+  ADD CONSTRAINT `san_pham_ibfk_1` FOREIGN KEY (`id_danh_muc`) REFERENCES `danh_muc` (`id_danh_muc`) ON DELETE RESTRICT ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
